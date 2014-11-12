6e4b7bcf7b22539313c9942bc2fe3b9314798310
Antoine Combes <antoine.combes@y-nov.com>
2014-11-12 12:07:03 +0100

    Class player

--- public/module/player.js
+++ public/module/player.js
@@ -1,1 +1,11 @@
+function Player(nom, x, y){
+    this.nom = nom;
+    this.alive = true;
+    this.pos = {
+        x:x,
+        y:y
+    };
+    this.speed = 0.45;
+    this.type = 'player';
 
+}
