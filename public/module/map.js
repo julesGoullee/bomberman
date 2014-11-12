85ea80a392b8f6cfe4fc6b3db634e0954399b4d7
Kévin Puythorac <kevin.puythorac@y-nov.com>
2014-11-12 12:09:05 +0100

    Ajout tableau dnas map.js

--- public/module/map.js
+++ public/module/map.js
@@ -1,4 +1,16 @@
 function Map(){
-    this.hauteur = 5;
+    this.largeur = 11;
+    this.longueur = 17;
+    this.content = [];
+    this.getplayers = function (){
+        var tabplayer = [];
+        var i = 0;
+        var size = this.content.length;
+        for (i;i<size;i++){
 
+        }
+    };
+    this.getplayerById = function (id){
+
+    };
 }
