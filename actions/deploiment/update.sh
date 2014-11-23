5b4c2c12534c5c300e00b5eee99893766bebe307
jules <julesgoullee@gmail.com>
2014-11-23 23:26:24 +0100

    deploiement

--- actions/deploiment/update.sh
+++ actions/deploiment/update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "$1" | sudo -S forever stopall
 echo "all process stop!" >&2
-# git pull &&
+git pull &&
 echo "Update project files!" >&2
 sudo forever start app/index.js
 echo "Nodejs app/index.js start!" >&2
