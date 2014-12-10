197b93463dc62363e9650eb9ab0c8f68ffaf7cdc
jules <julesgoullee@gmail.com>
2014-12-10 01:41:41 +0100

    launch.sh

--- actions/deploiment/launch.sh
+++ actions/deploiment/launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "$1" | sudo -S forever stopall
 echo "all process stop!" >&2
-sudo forever start app/server/index.js &&
+sudo forever start app/server/index.js -w &&
 echo "Nodejs app/server/index.js start!" >&2
 exit
