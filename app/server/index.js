82adef7b295e1cc84ab6f1953ec09c698e405c95
jules <julesgoullee@gmail.com>
2014-12-10 01:36:33 +0100

    update.sh

--- app/server/index.js
+++ app/server/index.js
@@ -3,7 +3,6 @@
 var config = require( "./config/config.js" );
 var express = require( "express" );
 var app = express();
-var server = require( config.rootPath + "./services/server/server.js" );
 var logService = require( config.rootPath + "./services/log/log.js" );
 var gameService = require( config.rootPath + "./modules/game/game.js" );
 
