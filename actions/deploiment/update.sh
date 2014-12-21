2a3478cae0ecbd7459406b92e67f69e917296864
jules G <julesgoullee@gmail.com>
2014-12-21 03:36:17 +0100

    root config prod necessaire

--- actions/deploiment/update.sh
+++ actions/deploiment/update.sh
@@ -3,8 +3,8 @@ echo "Git pull..." >&2
 git pull &&
 echo "Git pull [OK]" >&2
 echo "Configuration production..." >&2
-echo "$1" | sudo npm run prod &&
-echo "Configuration production [OK]" >&2
+npm run prod &&
+echo "Configuration production..." >&2
 echo "Update success :)" >&2
 exit
 
