e0583f0f95ea60f447d1298e43398b9c8a8264b3
jules <julesgoullee@gmail.com>
2015-07-02 01:11:35 +0200

    config prod

--- config/prod/config_prod.js
+++ config/prod/config_prod.js
@@ -3,11 +3,11 @@ var path = require("path");
 module.exports = {
     rootPath : "",
     rootPathPublic : path.resolve( __dirname + "/../../public/" ),
-    port : 443,
+    port : 4430,
     domaine: "monchezmoi.no-ip.org",
     maxPlayerPeerParty : 4,
     blockDim: 8,
-    showBlockTemp : false,
+    showBlockTemp : true,
     bombCountDown : 3000,
     showPowerUp : true,
     nbPowerUp : 50
