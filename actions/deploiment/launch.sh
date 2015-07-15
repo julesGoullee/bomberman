720fcc76d5d5c2d7700b64cd495398df196c6d39
julesGoullee <julesgoullee@gmail.com>
2015-07-15 10:53:41 +0200

    script launch forever logs

--- actions/deploiment/launch.sh
+++ actions/deploiment/launch.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-echo "All process stop..." >&2
-echo "$1" | sudo -S forever stopall &&
-echo "All process stop [OK]" >&2
-echo "Start node app..." >&2
-sudo forever start app/server/index.js -w &&
-echo "Start node app [OK]" >&2
-echo "Nodejs app/server/index.js start :)" >&2
+scriptDir="`dirname $0`"
+cd $scriptDir"/../../"
+mkdir -p log
+echo "Update sources..." &&
+git pull &&
+echo "Config prod" &&
+npm run prod &&
+echo "Start forever daemon" &&
+forever -o log/out.log -e log/err.log  --minUptime 500 --spinSleepTime 500 start app/server/index.js &&
+echo "Bombeman start :)"
 exit
