5619e14cdce8f2c012b0ddbd21a1d57f1ccecf46
julesGoullee <julesgoullee@gmail.com>
2015-07-22 17:01:48 +0200

    compress && max age

--- app/server/routes/static/staticRoutes.js
+++ app/server/routes/static/staticRoutes.js
@@ -4,13 +4,32 @@ var express = require( "express" );
 var config = require( "../../config/config.js" );
 var compress = require('compression');
 
+var contentTypeToCompress = [
+    'application/octet-stream',
+    'application/javascript',
+    'text/css',
+    'application/json'
+];
+
 function staticRoutes( app ) {
-    
+
     var rootPathPublic = "app/public" ;
 
     var oneDay = 86400000;
-    app.use( compress() );
-    app.use( express.static( rootPathPublic, { maxAge: oneDay } ) );
+
+    app.use(compress({filter: shouldCompress}));
+
+    function shouldCompress(req, res) {
+
+        var contentType = res.getHeader('Content-Type');
+
+        return contentTypeToCompress.indexOf(contentType) !== -1;
+    }
+
+    app.use( express.static( rootPathPublic, {
+        CacheControl: 'public, max-age=' + oneDay
+    }));
+
 }
 
 module.exports = function( app ) {
