5aa6ab936f9d2c3e49a1019f4e335b9d7b7dc440
julesGoullee <jgoullee.elevenlabs@tf1.fr>
2015-11-13 18:00:20 +0100

    test game globals var

--- serve/modules/game/test/testGame.js
+++ serve/modules/game/test/testGame.js
@@ -2,9 +2,10 @@
 
 var chai = require('chai');
 var sinonChai = require("sinon-chai");
-global.expect = chai.expect;
 global.sinon = require('sinon');
-chai.use(sinonChai);
+global.expect = chai.expect;
+global.assert = chai.assert;
+chai.use( sinonChai );
 var mock = require("../../test/mock.js");
 var utils = require("../../utils/utils.js");
 var config = require("../../../config/config.js");
