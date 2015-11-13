cbcf6ce65b38e91bfaee565e13813bb4ea56c475
julesGoullee <jgoullee.elevenlabs@tf1.fr>
2015-11-13 15:02:15 +0100

    indentation

--- client/src/modules/auth/auth.js
+++ client/src/modules/auth/auth.js
@@ -1,109 +1,110 @@
 "use strict";
 define(function(){
-    return function Auth( connector, popup ){
+  return function Auth( connector, popup ){
 
-        var self = this;
+    var self = this;
 
-        /*PUBLIC METHODS*/
+    /*PUBLIC METHODS*/
 
-        self.ready = function( callback ) {
+    self.ready = function( callback ) {
 
-            var token = getTokenLS();
+      var token = getTokenLS();
 
-            if ( getTokenLS() ){
+      if ( getTokenLS() ){
 
-                connector.setTokenAndReturnUseProfil( token, function( userProfil ) {
+        connector.setTokenAndReturnUseProfil( token, function( userProfil ) {
 
-                    if(  userProfil && !userProfil.err && userProfil.name && userProfil.token ) {
+          if(  userProfil && !userProfil.err && userProfil.name && userProfil.token ) {
 
-                        callback( userProfil );
-                    }
-                    else{
+            callback( userProfil );
+          }
+          else{
 
-                        console.log( "Token err: " + userProfil.err );
+            console.log( "Token err: " + userProfil.err );
 
-                        showForm( function( userProfil ){
+            showForm( function( userProfil ){
 
-                            popup.hide();
-                            callback( userProfil );
-                        });
-                    }
-                });
-
-            }
-            else{
-
-                showForm( function( userProfil ){
-
-                    popup.hide();
-                    callback( userProfil );
-                });
-            }
-        };
-
-        /*PRIVATE METHODS*/
-
-        function getTokenLS(){
-
-            return sessionStorage.getItem( "token" );
-        }
-
-        function setTokenLS( token ){
-
-            sessionStorage.setItem( "token", token );
-        }
-
-        function showForm( callback ){
-
-            var header = "<h4 class='modal-title' >Inscription</h4>";
-
-            var body = "<form id='form-disclamer'>"+
-                "<div class='form-group'>"+
-                "<input type='text' class='form-control' id='input-pseudo' placeholder='Entre ton pseudo de guerrier' required='true'>"+
-                "<span class='alert alert-warning' id='inscription-erreur' ></span>"+
-                "</div>"+
-                "<button type='submit' class='btn btn-default'>Jouer</button>"+
-                "</form>";
-
-
-            popup.setContent( header, body );
-
-            var errorContainer = $("#inscription-erreur");
-
-            popup.show();
-
-            connector.onSetUser( function( userProfil ){
-
-                if ( userProfil && !userProfil.err && userProfil.token && userProfil.name ) {
-
-                    setTokenLS( userProfil.token );
-
-                    callback( userProfil );
-                }
-                else{
-
-                    errorContainer.text( "Erreur: " + userProfil.err );
-
-                    errorContainer.show( "fast", function(){
-
-                        setTimeout(function(){
-
-                            errorContainer.hide("fast");
-
-                        },3000);
-
-                    });
-
-                }
-
+              popup.hide();
+              callback( userProfil );
             });
+          }
+        });
 
-            $("#form-disclamer").submit( function( e ) {
+      }
+      else{
 
-                e.preventDefault();
+        showForm( function( userProfil ){
 
-                connector.setUser( $("#input-pseudo").val());
-            });
-        }
+          popup.hide();
+          callback( userProfil );
+        });
+      }
     };
+
+    /*PRIVATE METHODS*/
+
+    function getTokenLS(){
+
+      return sessionStorage.getItem( "token" );
+    }
+
+    function setTokenLS( token ){
+
+      sessionStorage.setItem( "token", token );
+    }
+
+    function showForm( callback ){
+
+      var header = "<h4 class='modal-title' >Inscription</h4>";
+
+      var body = "<form id='form-disclamer'>"+
+        "<div class='form-group'>"+
+        "<input type='text' class='form-control' id='input-pseudo' placeholder='Entre ton pseudo de guerrier' required='true'>"+
+        "<span class='alert alert-warning' id='inscription-erreur' ></span>"+
+        "</div>"+
+        "<button type='submit' class='btn btn-default'>Jouer</button>"+
+        "</form>";
+
+
+
+      popup.setContent( header, body );
+
+      var errorContainer = $("#inscription-erreur");
+
+      popup.show();
+
+      connector.onSetUser( function( userProfil ){
+
+        if ( userProfil && !userProfil.err && userProfil.token && userProfil.name ) {
+
+          setTokenLS( userProfil.token );
+
+          callback( userProfil );
+        }
+        else{
+
+          errorContainer.text( "Erreur: " + userProfil.err );
+
+          errorContainer.show( "fast", function(){
+
+            setTimeout(function(){
+
+              errorContainer.hide("fast");
+
+            },3000);
+
+          });
+
+        }
+
+      });
+
+      $("#form-disclamer").submit( function( e ) {
+
+        e.preventDefault();
+
+        connector.setUser( $("#input-pseudo").val());
+      });
+    }
+  };
 });
