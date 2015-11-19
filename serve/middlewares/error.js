87cd9464e4ce92e0d7a98b33e7bb0ec7ae5e1ea7
julesGoullee <julesgoullee@gmail.com>
2015-11-19 14:57:29 +0100

    Error request handler log

--- serve/middlewares/error.js
+++ serve/middlewares/error.js
@@ -1,10 +1,12 @@
 "use strict"; /* jshint unused: false */
+var errorLog = require("../modules/log/log").error;
 
 module.exports = function(app) {
   if (process.env.NODE_ENV === "development") {
 
     app.use(function (err, req, res, next) {
 
+      throw err;
       res.status(500);
 
       res.json({
@@ -19,9 +21,10 @@ module.exports = function(app) {
   if (process.env.NODE_ENV === "production") {
 
     app.use(function (err, req, res, next) {
-      if (!res.statusCode) {
-        res.status(500);
-      }
+
+      errorLog.error(err.stack);
+
+      res.status(500);
 
       res.json({
         code: res.statusCode,
--- serve/modules/log/log.js
+++ serve/modules/log/log.js
@@ -4,9 +4,11 @@ var manager = require('simple-node-logger').createLogManager();
 var serveLogger = manager.createLogger('Serve');
 var bddLogger = manager.createLogger('Serve');
 var authLogger = manager.createLogger('Auth');
+var errorRequestHandlerLogger = manager.createLogger('Request handler');
 
 module.exports = {
   serve: serveLogger,
   bdd: bddLogger,
-  auth: authLogger
+  auth: authLogger,
+  error: errorRequestHandlerLogger
 };
