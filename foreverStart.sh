7b199386656ba9aa6f3c48a4ee8f65c8bd5a2b47
julesGoullee <julesgoullee@gmail.com>
2015-12-08 11:23:25 +0100

    Forever scripts start, gitignore log directory

---
+++ foreverStart.sh
@@ -0,0 +1,4 @@
+now=$(date +%Y'-'%m'-'%d'-'%H':'%M':'%S)
+forever stop serve/index.js &&
+mkdir -p log/$now && 
+NODE_ENV=production forever -o log/$now/out.log -e log/$now/err.log  --minUptime 500 --spinSleepTime 500 start serve/index.js
